e57f3fcaaf42f3714703bc62376309a748abd071
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-07-08 08:02:34 +0700

    ref(config): db connection with singleton pattern
    the simpleway with Object.freeze()

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("@prisma/client");
-var prisma = new client_1.PrismaClient();
+var prisma = Object.freeze(new client_1.PrismaClient());
 exports.default = prisma;
